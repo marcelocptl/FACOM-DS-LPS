7565fd305efab2f12dc20e9e8015753b2e1f39ab
BrucceNeves <brucce.neves@gmail.com>
2016-11-09 20:39:16 -0300

    Projeto de teste da biblioteca

---
+++ TesteProcessoJuridico/src/Teste.java
@@ -0,0 +1,16 @@
+
+import Model.Advogado;
+
+
+/**
+ *
+ * @author brucce
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+        Advogado adv = new Advogado();
+        System.out.println(adv);
+    }
+    
+}
